560b04cd9f16ce6b545dde63d5c4b07c1fbc05ec
Duy Nguyen <dunguyen@adobe.com>
2020-12-16 18:17:47 +0100

    Use Photoshop SDK for API calls

--- actions/autotone/index.js
+++ actions/autotone/index.js
@@ -18,76 +18,37 @@
 
 const { worker, SourceCorruptError } = require('@adobe/asset-compute-sdk')
 const fs = require('fs').promises
-const { downloadFile } = require('@adobe/httptransfer')
 const { Files } = require('@adobe/aio-sdk')
+const Photoshop = require('@adobe/aio-lib-photoshop-api')
 const { v4: uuid4 } = require('uuid')
 
 exports.main = worker(async (source, rendition, params) => {
-    // Example of how to throw a standard asset compute error
-    // if e.g. the file is empty or broken.
     const stats = await fs.stat(source.path)
     if (stats.size === 0) {
         throw new SourceCorruptError('source file is empty')
     }
+    
+    const files = await Files.init()
 
     const accessToken = params.auth && params.auth.accessToken
+    const orgId = params.auth && params.auth.orgId
+    // init Photoshop SDK client
+    const psClient = await Photoshop.init(orgId, params.apiKey, accessToken, files)
+
+    // create a new directory in aio-lib-files with unique name
     const imageId = uuid4()
+    const aioSourcePath = `${imageId}/source.png`
+    const aioRenditionPath = `${imageId}/rendition.png`
 
-    const files = await Files.init()
-    await files.copy(source.path, `${imageId}/source.png`, { localSrc: true })
-    const downloadUrl = await files.generatePresignURL(`${imageId}/source.png`, { expiryInSeconds: 600})
-    const uploadUrl = await files.generatePresignURL(`${imageId}/rendition.png`, { expiryInSeconds: 600, permissions: 'rwd' })
+    await files.copy(source.path, aioSourcePath, { localSrc: true })
 
-    const apiEndpoint = 'https://image.adobe.io/lrService/autoTone'
+    // call Photoshop API to do autotone processing, and poll status until it's successful
+    const result = await psClient.autoTone(aioSourcePath, aioRenditionPath)
+    await result.pollUntilDone(1000)
 
-    const options = {
-      inputs: {
-        href: downloadUrl,
-        storage: 'external'
-      },
-      outputs: [
-        {
-          href: uploadUrl,
-          storage: 'azure'
-        }
-      ]
-    }
+    // download the rendition to local AEM Assets destination
+    await files.copy(aioRenditionPath, rendition.path, { localDest: true })
 
-    const reqHeaders = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${accessToken}`,
-      'x-api-key': params.apiKey
-    }
-
-    // fetch content from external api endpoint
-    const res = await fetch(apiEndpoint, {
-      method: 'POST',
-      headers: reqHeaders,
-      body: JSON.stringify(options)
-    })
-    if (!res.ok) {
-      throw new Error('request to ' + apiEndpoint + ' failed with status code ' + res.status)
-    }
-    const content = await res.json()
-
-    if (!content._links || !content._links.self || !content._links.self.href) {
-      throw new Error('Photoshop API did not return expected value.')
-    }
-
-    let processed = false
-    while (!processed) {
-      // sleep 1s before the enterring loop
-      await new Promise(r => setTimeout(r, 1000))
-      const statusRes = await fetch(content._links.self.href, { headers: reqHeaders })
-      const statusContent = await statusRes.json()
-      const outputs = statusContent.outputs
-      if (outputs.length > 0 && outputs[0].status === 'succeeded') {
-        processed = true
-      }
-    }
-
-    await downloadFile(uploadUrl, rendition.path)
-
-    // clean up files processing folder
+    // clean up files processing folder in aio-lib-files
     await files.delete(`${imageId}/`)
 })
--- actions/cutout/index.js
+++ actions/cutout/index.js
@@ -18,76 +18,37 @@
 
 const { worker, SourceCorruptError } = require('@adobe/asset-compute-sdk')
 const fs = require('fs').promises
-const { downloadFile } = require('@adobe/httptransfer')
 const { Files } = require('@adobe/aio-sdk')
+const Photoshop = require('@adobe/aio-lib-photoshop-api')
 const { v4: uuid4 } = require('uuid')
 
 exports.main = worker(async (source, rendition, params) => {
-    // Example of how to throw a standard asset compute error
-    // if e.g. the file is empty or broken.
     const stats = await fs.stat(source.path)
     if (stats.size === 0) {
         throw new SourceCorruptError('source file is empty')
     }
 
-    let accessToken = params.auth && params.auth.accessToken
-    const imageId = uuid4()
-
     const files = await Files.init()
-    await files.copy(source.path, `${imageId}/source.png`, { localSrc: true })
-    const downloadUrl = await files.generatePresignURL(`${imageId}/source.png`, { expiryInSeconds: 600})
-    const uploadUrl = await files.generatePresignURL(`${imageId}/rendition.png`, { expiryInSeconds: 600, permissions: 'rwd' })
-    
-    const apiEndpoint = 'https://image.adobe.io/sensei/cutout'
 
-    const options = {
-      input: {
-        href: downloadUrl,
-        storage: 'external'
-      },
-      output: {
-        href: uploadUrl,
-        storage: 'azure',
-        mask:{
-          format: 'soft'
-        }
-      }
-    }
+    const accessToken = params.auth && params.auth.accessToken
+    const orgId = params.auth && params.auth.orgId
+    // init Photoshop SDK client
+    const psClient = await Photoshop.init(orgId, params.apiKey, accessToken, files)
 
-    const reqHeaders = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${accessToken}`,
-      'x-api-key': params.apiKey
-    }
+    // create a new directory in aio-lib-files with unique name
+    const imageId = uuid4()
+    const aioSourcePath = `${imageId}/source.png`
+    const aioRenditionPath = `${imageId}/rendition.png`
 
-    // fetch content from external api endpoint
-    const res = await fetch(apiEndpoint, {
-      method: 'POST',
-      headers: reqHeaders,
-      body: JSON.stringify(options)
-    })
-    if (!res.ok) {
-      throw new Error('request to ' + apiEndpoint + ' failed with status code ' + res.status)
-    }
-    const content = await res.json()
+    await files.copy(source.path, aioSourcePath, { localSrc: true })
 
-    if (!content._links || !content._links.self || !content._links.self.href) {
-      throw new Error('Photoshop API did not return expected value.')
-    }
+    // call Photoshop API to do cutout processing, and poll status until it's successful
+    const result = await psClient.createCutout(aioSourcePath, aioRenditionPath)
+    await result.pollUntilDone(1000)
 
-    let processed = false
-    while (!processed) {
-      // sleep 1s before the enterring loop
-      await new Promise(r => setTimeout(r, 1000))
-      const statusRes = await fetch(content._links.self.href, { headers: reqHeaders })
-      const statusContent = await statusRes.json()
-      if (statusContent.status === 'succeeded') {
-        processed = true
-      }
-    }
+    // download the rendition to local AEM Assets destination
+    await files.copy(aioRenditionPath, rendition.path, { localDest: true })
 
-    await downloadFile(uploadUrl, rendition.path)
-
-    // clean up files processing folder
+    // clean up files processing folder in aio-lib-files
     await files.delete(`${imageId}/`)
 })
